daf977ff7d63957dfbf6e03a85c8db1e6852c9d6
srinath <madusanka525@gmail.com>
2015-05-19 11:58:40 +0530

    files add

---
+++ custom.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+<script src="http://code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css" />
+
+<!--plugin js file and css file-->
+<script src="datepicker-customdate-heighlighter.min.js"></script>
+<link rel="stylesheet"  type="text/css" href="datepicker-customdate-heighlighter.css" />
+
+
+
+<script>
+$(document).ready(function(){
+	$( ".selectoraa" ).highlightdater('dates.csv');  //
+	});
+
+
+
+</script>
+
+
+
+
+
+
+
+</head>
+
+
+
+
+
+
+<body>
+ <input class="selectoraa" type="text">
+</body>
+</html>
